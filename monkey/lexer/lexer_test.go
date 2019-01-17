97c35ea2279ce447c1a61da34db232c87c8966d1
naotoyoshimoto <namusic7010@gmail.com>
2019-01-17 18:18:58 +0900

    add skip white space fn

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -9,10 +9,10 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 	let ten = 10;
-	let add = fn(x,y) {
+	let add = fn(x, y) {
 		x + y;
 	};
-	let result = add(five,ten)
+	let result = add(five, ten);
 	`
 
 	tests := []struct {
@@ -63,6 +63,11 @@ func TestNextToken(t *testing.T) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 				i, tt.expectedLiteral, tok.Literal)
 		}
