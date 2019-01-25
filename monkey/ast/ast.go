9ee57bbbdcdcb3637055df618bc42e5a71a28d75
naotoyoshimoto <namusic7010@gmail.com>
2019-01-25 13:11:44 +0900

    add boolean literal test

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -81,6 +81,11 @@ type InfixExpression struct {
 	Right    Expression
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
@@ -158,3 +163,7 @@ func (oe *InfixExpression) String() string {
 	return out.String()
 
 }
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
