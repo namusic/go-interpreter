ea9dccde3fc6988c8e284b6e024ecb88af2d1023
naotoyoshimoto <namusic7010@gmail.com>
2019-01-31 12:12:24 +0900

    add implement bool evaluation

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/go-interpreter/monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -16,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -27,4 +35,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
