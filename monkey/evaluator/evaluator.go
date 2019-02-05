5f9f67b4f5d157265df1806a690796f3e4f69015
naotoyoshimoto <namusic7010@gmail.com>
2019-02-05 12:41:58 +0900

    add enclosed environment

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -80,6 +80,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
 
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+
+		}
 	}
 
 	return nil
@@ -249,4 +259,20 @@ func evalIdentifier(
 		return newError("identifier not found: " + node.Value)
 	}
 	return val
+}
+
+func evalExpressions(
+	exps []ast.Expression,
+	env *object.Environment,
+) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
