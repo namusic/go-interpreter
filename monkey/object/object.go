822ae50920a12a2c7ecbe427a1f668e9626c3d23
naotoyoshimoto <namusic7010@gmail.com>
2019-02-07 12:35:48 +0900

    add fix builtin object and test len builtin fn

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -94,9 +94,10 @@ type String struct {
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
 
+type BuiltinFunction func(args ...Object) Object
 type Builtin struct {
 	Fn BuiltinFunction
 }
 
-func (b *Builtin) Type() objectType { return BUILTIN_OBJ }
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
