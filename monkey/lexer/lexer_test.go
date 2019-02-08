efcac011c9f06a539f1fbc4505ea2a78d0f211ce
naotoyoshimoto <namusic7010@gmail.com>
2019-02-08 11:41:48 +0900

    add impl lexer to be informed array by RACKET

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -26,7 +26,7 @@ func TestNextToken(t *testing.T) {
 	10 != 9;
 	"foobar"
 	"foo bar"
-	"[1, 2];"
+	[1, 2];
 	`
 
 	tests := []struct {
@@ -108,11 +108,11 @@ func TestNextToken(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
-		{token.LBRACLET, "["},
+		{token.LBRACKET, "["},
 		{token.INT, "1"},
 		{token.COMMA, ","},
 		{token.INT, "2"},
-		{token.RBRACLET, "]"},
+		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
